2e64fd79e11dfec62b0869cb459aa52f1ca70523
chelsealuttrall <chelsealuttrall@gmail.com>
2019-12-28 16:47:24 -0700

    done with html & css for the night

---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+//Global Variables
+
+
+//My array of questions, answers, possible fake answers
+
+Question 1
+q
+a
+p
+
+Question 2
+q
+a
+p
+
+Question 3
+q
+a
+p
+
+Question 4
+q
+a
+p
+
+Question 5
+q
+a
+p
+
+Question 6
+q
+a
+p
+
+Question 7
+q
+a
+p
+
+Question 8
+q
+a
+p
+
+Question 9
+q
+a
+p
+
+Question 10
+q
+a
+p
+
+
+// Loop through questions
+
+//On click function for selection
+
+//Determine Correct/Wrong
+
+//Tally Score
+
+
+//<span id="questions"></span>
+//<span id="choices"></span>
+//<span id="tidbit"></span>
